3b9cf07df18cd90b0fe21738346f5f0c561ca974
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2018-05-03 15:31:06 +0300

    Fitting functions now return L. Modified documentation to include outputs

--- bayesfit/fitting.py
+++ bayesfit/fitting.py
@@ -8,7 +8,8 @@ def outlier_fit(f_model,p0,x,y,sigma0,method='conservative'):
     '''
     Least squares fitting algorithm with outlier handling. Fits the given model
     f_model(x,*p) to the (x,y) data. The initial guess for the parameters p0 is
-    to be given.
+    to be given. Returns optimized p, covariance matrix cov, and the likelihood
+    function L.
 
     sigma0 has a slightly different function dependending on the used method.
     With 'conservative' sigma0 is the lower bound for the uncertainty of each
@@ -31,12 +32,13 @@ def outlier_fit(f_model,p0,x,y,sigma0,method='conservative'):
             return -np.sum(np.log(1+0.5*R**2))
 
     p, cov = maximize_likelihood(L,p0)
-    return p, cov
+    return p, cov, L
 
 def least_squares(f_model,p0,x,y,yerr=1,noise_scaling=False):
     '''
     Least squares fitting. Fits the given model f_model(x,*p) to the (x,y) data.
-    The initial guess for the parameters p0 is to be given.
+    The initial guess for the parameters p0 is to be given. Returns optimized p,
+    covariance matrix cov, and the likelihood function L.
 
     The uncertainties in y yerr can be either a float or an array.
     If error_scaling is False, then the fit is an ordinary least squares.
@@ -51,9 +53,9 @@ def least_squares(f_model,p0,x,y,yerr=1,noise_scaling=False):
     p, cov = maximize_likelihood(L,p0)
 
     if noise_scaling == False:
-        return p, cov
+        return p, cov, L
     else:
         #Allowing errors to scale corresponds to maximizing L = - N/2*ln(chi^2).
         #This leads to the same optimal as with the ordinary case but with
         #scaled covariance matrix
-        return p, -cov*2*L(p)/y.size
+        return p, -cov*2*L(p)/y.size, L
